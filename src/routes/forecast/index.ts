a78b7ec8496fda9437ccdb002a308d6764fe892e
Stanislav <stanislav@qminderapp.com>
2022-04-17 22:30:12 +0300

    feat: add we magnitude

--- src/routes/forecast/index.ts
+++ src/routes/forecast/index.ts
@@ -1,8 +1,13 @@
 import {FastifyInstance} from 'fastify'
 import getForecast from './get-forecast';
 import postCity from './post-city';
+import wsMagnitude from './ws-magnitude';
 
 export default async function (fastify: FastifyInstance) {
     fastify.route(getForecast(fastify));
     fastify.route(postCity(fastify));
+    fastify.get('/magnitude/:city',
+        { websocket: true },
+        (conn, req) => wsMagnitude(conn, req)
+    );
 }
